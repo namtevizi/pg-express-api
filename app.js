25048a837bd54a00c8f91c406999bb35058626d5
Nam Tran <namtvh@gmail.com>
2019-03-19 06:33:20 +0700

    The initial version

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import routes from './server/routes';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express() // setup express application
+
+app.use(logger('dev')); // log requests to the console
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+routes(app);
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the default API route',
+}));
+
+app.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
